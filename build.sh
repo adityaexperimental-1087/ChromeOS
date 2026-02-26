94a845c48b9a8a7b9dd5018489df50e9a72c1681
Aditya <adityaexperimental@gmail.com>
2026-02-26 13:00:04 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -22,7 +22,7 @@ clean_previous_run() {
 # Function to download Chrome OS
 download_chromeos() {
     local code_name=$1
-    local url="https://cros.download/recovery/samus"
+    local url="https://cros.download/recovery/rammus"
     local response=$(curl -s --progress-bar "$url")
     local link=$(echo "$response" | sed -n 's/.*<a[^>]*href="\([^"]*dl\.google\.com[^"]*\.zip\)"[^>]*>\([^<]*\)<\/a>.*/\1 \2/p' | awk '
     {
